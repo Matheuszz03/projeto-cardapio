634348f00950d8cae0edc45244da3e94d8111478
Matheus Silva <121463710+Matheuszz03@users.noreply.github.com>
2023-03-16 22:10:10 -0300

    att

--- js/script.js
+++ js/script.js
@@ -8,3 +8,22 @@ menu.addEventListener("click", function () {
 window.onscroll = () => {
     navbar.classList.remove("active");
 }
+
+// Scroll suave 
+// const menuItens = document.querySelectorAll(".bx-menu a");
+
+// menuItens.forEach(item => {
+//     item.addEventListener('click', scrollToIdOnClick);
+// })
+
+// function scrollToIdOnClick(event) {
+//     event.preventDefault();
+//     const element = event.target;
+//     const id = element.getAttribute('href');
+//     const to = document.querySelector(id).offsetTop;
+
+//     window.scroll({
+//         top: to;
+//         behavior: "smooth",
+//     });
+// }
